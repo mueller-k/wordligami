ecac2d3982171cb7e44d353be97737cef00554e1
Kyle Mueller <Mueller.Kyle@principal.com>
2022-02-06 23:18:18 -0600

    init

--- lib/wordligami-stack.ts
+++ lib/wordligami-stack.ts
@@ -1,16 +1,28 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as api from 'aws-cdk-lib/aws-apigateway';
+
+import path = require("path");
+import { ApiGateway } from 'aws-cdk-lib/aws-events-targets';
 
 export class WordligamiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const wordligamiFunction = new lambda.Function(this, ' WordligamiFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, "..", "src"))
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'WordligamiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const wordligamiApi = new api.LambdaRestApi(this, 'WordligamiApi', {
+      handler: wordligamiFunction,
+      proxy: false
+    })
+
+    const boards = wordligamiApi.root.addResource("board")
+    boards.addMethod('GET');
+    boards.addMethod('POST');
   }
 }
